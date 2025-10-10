899935b357887f3d6e79cb54472bee05a931a28c
liasse-clb <162380208+liasse-clb@users.noreply.github.com>
2025-10-10 00:18:24 -0300

    Atividades 1,2 e 3 concluídas

---
+++ src/Ex001.java
@@ -0,0 +1,9 @@
+class HelloWord {
+    public static void main(String[] args) {
+        System.out.println('A');
+        System.out.println(10);
+        System.out.println("Programação orientada a objetos");
+    }
+}
+
+// Objetivo: imprimir 1 caractere, imprimir 1 número e 1 frase
---
+++ src/Ex003.java
@@ -0,0 +1,16 @@
+//Imprimir um as iniciais do meu nome
+class IniciaisDoNome {
+    public static void main(String[] args) {
+        System.out.println("    ***********       ***                ************         ");
+        System.out.println("  ****************    ***               **************        ");
+        System.out.println("****                  ***               ***         ***       ");
+        System.out.println("***                   ***               *************         ");
+        System.out.println("**                    ***               ************          ");
+        System.out.println("**                    ***               **************        ");
+        System.out.println("****                  ***               ***          **       ");
+        System.out.println("  ****************    ***************   **************        ");
+        System.out.println("    ***********       ***************    ************         ");
+    }
+
+}
+
