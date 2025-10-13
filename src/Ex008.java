7cc8bfd013ce62ef64ff8dc15a4934bc79bfab21
liasse-clb <162380208+liasse-clb@users.noreply.github.com>
2025-10-12 23:33:15 -0300

    Terminei a atividade de manipulação de String com inversão de dados

---
+++ src/Ex008.java
@@ -0,0 +1,14 @@
+class HelloGoodbye {
+    public static void main(String[] args) {
+        String name1 = args[0];
+        String name2 = args[1];
+        String name3 = args[2];
+        String name4 = args[3];
+
+        System.out.println("Hello " + name1 + " and " + name2 + ".");
+        System.out.println("Goodbye " + name2 + " and " + name1 + ".");
+
+        System.out.println("Hello " + name3 + " and " + name4 + ".");
+        System.out.println("Goodbye " + name4 + " and " + name3 + ".");
+    }
+}
